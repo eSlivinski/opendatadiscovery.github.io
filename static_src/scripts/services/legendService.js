7f3600a727f35e19ff40c70b8de995f330dfae6f
eSlivinski <eslivinski@gmail.com>
2016-03-15 20:04:14 -0400

    d3 legend stuff :)

--- static_src/scripts/services/legendService.js
+++ static_src/scripts/services/legendService.js
@@ -7,47 +7,71 @@ app.service('legendService', function($rootScope) {
 
   var colors = this.colors = ['#ffffcc', '#a1dab4', '#41b6c4', '#2c7fb8', '#253494'];
 
+  var padding = { left: 60, top: 10, right: 10, bottom: 30 };
+  var h = 210 - (padding.top + padding.bottom),
+      w = 350 - (padding.left + padding.right) ;
+
   var updateLegend = function(stat, type) {
       var sum = _.sum(stat);
 
       var xscale = d3.scale.linear()
-              .domain([_.min(stat), _.max(stat)])
-              .range([0, 800]);
-
-      var	xAxis = d3.svg.axis();
-  		    xAxis
-            .orient('bottom')
-    				.scale(xscale);
+              .domain([0, _.max(stat)/sum])
+              .range([0, w]);
 
       var yscale = d3.scale.linear()
   						.domain([0, stat.length - 1])
-  						.range([0, 480]);
+  						.range([h, 0]);
 
-      var	yAxis = d3.svg.axis();
-    			yAxis
-    				.orient('left')
-    				.scale(yscale)
-    				.tickValues(function(d, i) {
-                if (i < limits[type].length) {
-                    return '< ' + limits[type][i].toString();
-                } else {
-                    return '>' + limits[type][limits[type].length - 1].toString();
-                }
-            });
+      // Define Labels
+      var yTicks = _.map(stat, function(d, i) {
+                        if (i < limits[type].length) {
+                            return '< ' + limits[type][i].toString();
+                        } else {
+                            return '> ' + limits[type][limits[type].length - 1].toString();
+                        }
+                    });
 
-      var cavans = d3.select('#legend')
-          .selectAll('div')
-          .data(stat)
-          .enter()
-          .append('div')
-              .style('width', function(value) { return value / sum * 600 + 'px'; })
-              .style('background-color', function(value, index) { return colors[index]; })
-          .append('span')
-              .text(function(value) { return value; });
-          // .append('g')
-          //     .call(yAxis)
-          // .append('g')
-          //     .call(xAxis);
+      var	xAxis = d3.svg.axis()
+              .orient('bottom')
+    				  .scale(xscale);
+
+      // Create SVG
+      var svg = d3.select('#legend')
+              .append('svg')
+              .attr('height', h + (padding.top + padding.bottom))
+              .attr('width', w + (padding.left + padding.right));
+
+          // Add x axis
+          svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(" + [ padding.left,  (h + padding.top) ]  + ")")
+                .call(xAxis);
+
+      // Create container within svg that is adjusted to specified padding
+      var g = svg.append('g')
+              .attr('height', h)
+              .attr('width', w)
+              .attr('transform', function() {
+                return 'translate (' + [padding.left, padding.top  ] + ')'
+              });
+
+      var bars = g.selectAll('.bars')
+              .data(stat)
+              .enter().append('rect')
+              .attr('width', function(d) { return xscale(d/sum); })
+              .attr('height', (h/stat.length - 10))
+              .attr('x', 0)
+              .attr('y', function(d, i) { return i * (h/stat.length) })
+              .attr('fill', function(d, i) { return colors[i]; });
+
+      var labels = svg.selectAll('.y-labels')
+              .data(stat)
+              .enter().append('text')
+              .text(function(d, i) { return yTicks[i]; })
+              .attr('x', 0)
+              .attr('y', function(d, i) { return (i * (h/stat.length)) + ((h/stat.length - 10)/2) })
+              .attr('class', 'y-labels')
+              .attr('dy', '1.2em');
   };
 
   $rootScope.$on('state_map:loaded', function(event, data) {
